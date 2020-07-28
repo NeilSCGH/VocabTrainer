48bd021a2fae72c9d0adbcc193ccfd185e2f90fd
NeilSCGH <neil.segard@gmail.com>
2020-07-29 00:11:55 +0200

    generate valid question and check answer

--- VocabTrainer/MainWindow.xaml.cs
+++ VocabTrainer/MainWindow.xaml.cs
@@ -11,40 +11,84 @@ namespace VocabTrainer
 
         List<string> questions = new List<string>();
         List<string> answers = new List<string>();
+
+        private int[] answersIds = new int[4];
+        private int realAnswer, nb;
+        private Random rng = new Random();
+
         public MainWindow()
         {
+            string[] lines, tmp;
+
             InitializeComponent();
 
-            string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension("words", ".csv"));
-            
+            lines = File.ReadAllLines(Path.ChangeExtension("words", ".csv"));
+            nb = lines.Length;
+
             foreach (string line in lines)
             {
-                string[] tmp = line.Split(';');
+                tmp = line.Split(';');
                 questions.Add(tmp[0]);
                 answers.Add(tmp[1]);
             }
-            
 
+            newQuestion();
+        }
+
+        private void newQuestion()
+        {
+            answersIds = new int[4];
+
+            answersIds[0] = rng.Next(nb);
+
+            do{answersIds[1] = rng.Next(nb);
+            } while (answersIds.Contains(answersIds[1]));
+
+            do{answersIds[2] = rng.Next(nb);
+            } while (answersIds.Contains(answersIds[2]));
+            
+            do{answersIds[3] = rng.Next(nb);
+            } while (answersIds.Contains(answersIds[3]));
+
+            realAnswer = rng.Next(4);
+
+            question.Content = questions[answersIds[realAnswer]];
+            answer1.Content = answers[answersIds[0]];
+            answer2.Content = answers[answersIds[1]];
+            answer3.Content = answers[answersIds[2]];
+            answer4.Content = answers[answersIds[3]];
+        }
+
+        private void result(int answerClicked)
+        {
+            if (answerClicked == realAnswer)
+            {
+                MessageBox.Show("True");
+            }
+            else
+            {
+                MessageBox.Show("False");
+            }
         }
 
         private void Answer1_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("neil");
+            result(0);
         }
 
         private void Answer2_Click(object sender, RoutedEventArgs e)
         {
-
+            result(1);
         }
 
         private void Answer3_Click(object sender, RoutedEventArgs e)
         {
-
+            result(2);
         }
 
         private void Answer4_Click(object sender, RoutedEventArgs e)
         {
-
+            result(3);
         }
     }
 }
