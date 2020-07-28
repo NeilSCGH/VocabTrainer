d14ef1c08c576afd8f20735fe8d74ecb21d8bef4
NeilSCGH <neil.segard@gmail.com>
2020-07-29 01:12:33 +0200

    points added, reverse languages button added

--- VocabTrainer/MainWindow.xaml.cs
+++ VocabTrainer/MainWindow.xaml.cs
@@ -9,30 +9,42 @@ namespace VocabTrainer
     public partial class MainWindow : Window
     {
 
-        List<string> questions = new List<string>();
-        List<string> answers = new List<string>();
+        List<string> questions;
+        List<string> answers;
 
         private int[] answersIds = new int[4];
         private int realAnswer, nb;
         private Random rng = new Random();
+        private string[] rawData;
+        private int points;
+
+        private bool reverse;
 
         public MainWindow()
         {
-            string[] lines, tmp;
-
             InitializeComponent();
 
-            lines = File.ReadAllLines(Path.ChangeExtension("words", ".csv"));
-            nb = lines.Length;
+            rawData = File.ReadAllLines(Path.ChangeExtension("words", ".csv"));
+            nb = rawData.Length;
+            reverse = false;
+            points = 0;
 
-            foreach (string line in lines)
+            generateQA();
+            newQuestion();
+        }
+
+        private void generateQA()
+        {
+            string[] tmp;
+            questions = new List<string>();
+            answers = new List<string>();
+
+            foreach (string line in rawData)
             {
                 tmp = line.Split(';');
-                questions.Add(tmp[0]);
-                answers.Add(tmp[1]);
+                questions.Add(tmp[reverse ? 1 : 0]);
+                answers.Add(tmp[reverse ? 0 : 1]);
             }
-
-            newQuestion();
         }
 
         private void newQuestion()
@@ -63,32 +75,27 @@ namespace VocabTrainer
         {
             if (answerClicked == realAnswer)
             {
-                MessageBox.Show("True");
+                newQuestion();
+                points += 1;
+                labelPtn.Content = "Points : " + points.ToString();
+                //MessageBox.Show("True");
             }
             else
             {
-                MessageBox.Show("False");
+                MessageBox.Show("Wrong!");
             }
         }
 
-        private void Answer1_Click(object sender, RoutedEventArgs e)
-        {
-            result(0);
-        }
+        private void Answer1_Click(object sender, RoutedEventArgs e) { result(0); }
+        private void Answer2_Click(object sender, RoutedEventArgs e) { result(1); }
+        private void Answer3_Click(object sender, RoutedEventArgs e) { result(2); }
+        private void Answer4_Click(object sender, RoutedEventArgs e) { result(3); }
 
-        private void Answer2_Click(object sender, RoutedEventArgs e)
-        {
-            result(1);
-        }
-
-        private void Answer3_Click(object sender, RoutedEventArgs e)
-        {
-            result(2);
-        }
-
-        private void Answer4_Click(object sender, RoutedEventArgs e)
-        {
-            result(3);
+        private void reverseBtn_Click(object sender, RoutedEventArgs e) 
+        { 
+            reverse = !reverse;
+            generateQA();
+            newQuestion();
         }
     }
 }
