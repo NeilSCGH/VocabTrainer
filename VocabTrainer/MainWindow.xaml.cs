ea62af3d76c511e3b784397ca3f608b58be5fcae
NeilSCGH <neil.segard@gmail.com>
2020-07-28 23:48:48 +0200

    getting the data from the csv

--- VocabTrainer/MainWindow.xaml.cs
+++ VocabTrainer/MainWindow.xaml.cs
@@ -1,29 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VocabTrainer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<string> questions = new List<string>();
+        List<string> answers = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
+
+            string[] lines = File.ReadAllLines(System.IO.Path.ChangeExtension("words", ".csv"));
+            
+            foreach (string line in lines)
+            {
+                string[] tmp = line.Split(';');
+                questions.Add(tmp[0]);
+                answers.Add(tmp[1]);
+            }
+            
+
         }
 
+        private void Answer1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("neil");
+        }
+
+        private void Answer2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Answer3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Answer4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
